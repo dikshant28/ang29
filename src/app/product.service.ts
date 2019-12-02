5047de0f43ab30ac153fd26eabbea19cb70c576e
dikshant28 <58214551+dikshant28@users.noreply.github.com>
2019-12-02 15:28:59 +0530

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductService {
 
-  url ='http://192.168.0.105:9898/product'
+  url ='http://172.18.7.26:9898/product'
   
   constructor(public http:HttpClient) { }
 
